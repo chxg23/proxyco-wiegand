b468b25d43a1177ee45d8784169d0ad45252a1a0
Jeremy Wood <jeremy@proxy.co>
2018-07-05 18:09:49 -0700

    Added wiegand_write(), which will write out wiegand pulses. Added handles for adjusting pulse timing, etc.

--- wiegand/include/wiegand/wiegand.h
+++ wiegand/include/wiegand/wiegand.h
@@ -1,5 +1,6 @@
 #define WIEGAND_NO_GPIO 0xFF
 
 void wiegand_init(void);
+void wiegand_write(uint32_t wiegand_bits, uint8_t *wiegand_data, uint8_t len);
 
 void wiegand_toggle(void);
--- wiegand/src/wiegand.c
+++ wiegand/src/wiegand.c
@@ -11,26 +11,49 @@
 #include "console/console.h"
 #include "wiegand/wiegand_log.h"
 
-/* Pkg init function */
+int g_d0_pin;
+int g_d1_pin;
+
+
+/* Initialize wiegand output, using pins d0_pin and d1_pin. The  */
 void
 wiegand_init(void)
 {
   int rc = 0;
 
-  rc = hal_gpio_init_out(MYNEWT_VAL(WIEGAND_D0_PIN), HAL_GPIO_MODE_OUT);
+  g_d0_pin = MYNEWT_VAL(WIEGAND_D0_PIN);
+  g_d1_pin = MYNEWT_VAL(WIEGAND_D1_PIN);
+
+  rc = hal_gpio_init_out(g_d0_pin, HAL_GPIO_PULL_NONE);
   assert(rc == 0);
 
-  rc = hal_gpio_init_out(MYNEWT_VAL(WIEGAND_D1_PIN), HAL_GPIO_MODE_OUT);
+  rc = hal_gpio_init_out(g_d1_pin, HAL_GPIO_PULL_NONE);
   assert(rc == 0);
 
   console_printf("wiegand_init: D0: %d, D1: %d. Ready\n",
-      MYNEWT_VAL(WIEGAND_D0_PIN),
-      MYNEWT_VAL(WIEGAND_D1_PIN));
+      g_d0_pin,
+      g_d1_pin);
 }
 
 void
-wiegand_toggle(void)
+wiegand_write(uint32_t wiegand_bits, uint8_t *wiegand_data, uint8_t len)
 {
-  hal_gpio_toggle(MYNEWT_VAL(WIEGAND_D0_PIN));
-  hal_gpio_toggle(MYNEWT_VAL(WIEGAND_D1_PIN));
+  int pin_to_pulse;
+
+  for (int i = 0; i < wiegand_bits; i++)
+  {
+    if ( (wiegand_data[i / 8] & (1 << (i % 8) )) != 0 )
+    {
+      pin_to_pulse = g_d1_pin;
+    }
+    else
+    {
+      pin_to_pulse = g_d0_pin;
+    }
+
+    hal_gpio_write(pin_to_pulse, 1);
+    os_cputime_delay_usecs(MYNEWT_VAL(WIEGAND_PULSE_LEN));
+    hal_gpio_write(pin_to_pulse, 0);
+    os_cputime_delay_usecs(MYNEWT_VAL(WIEGAND_PERIOD_LEN));
+  }
 }
